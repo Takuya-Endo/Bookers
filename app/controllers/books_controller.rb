e595ec38deeb78cf79472c2f71177880e0182fb8
Takuya-Endo <above5purple@gmail.com>
2021-02-07 02:56:16 +0000

    [Add]新規投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,31 @@
 class BooksController < ApplicationController
-  
+
   def top
     # トップページ表示
   end
 
   def index
+
+    # newアクション（新規投稿フォーム）は、indexアクション（一覧表示「/books」）同一ページ内
+    @book = Book.new  # indexアクション（一覧表示「/books」）実行時に空のモデルを代入
   end
+
+    def create  # 新規投稿フォームの入力内容をデータベースへ追加
+      book = Book.new(book_params)
+      book.save
+      # 仮のリダイレクト先
+      redirect_to books_path
+    end
 
   def show
   end
 
   def edit
   end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
+  #     books GET    /books(.:format)           books#index
+  #           POST   /books(.:format)           books#create
+  #  new_book GET    /books/new(.:format)       books#new
+  # edit_book GET    /books/:id/edit(.:format)  books#edit
+  #      book GET    /books/:id(.:format)       books#show
+  #           PATCH  /books/:id(.:format)       books#update
+  #           PUT    /books/:id(.:format)       books#update
+  #           DELETE /books/:id(.:format)       books#destroy
+
   root :to => 'books#top' # トップ画面をルートパスに設定
 end
