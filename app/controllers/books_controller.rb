a7ba5814ec7c865d8d4726cee19b0abd9182d591
Takuya-Endo <above5purple@gmail.com>
2021-02-08 11:32:30 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     if book.save  #titleとbodyのpresenceがtrueの場合は、saveとredirect
       flash[:notice] = "Book was successfully created."  #サクセスメッセージ
-      redirect_to book_path(book.id)  # <!--名前付きルート「as:'book'」→books#show→show.html.erb-->
+      redirect_to book_path(book.id)  #名前付きルート「as:'book'」→books#show→show.html.erb
     else  #presenceがfalseであれば、エラー表示と共にindexアクション再実行
       @books ||= Book.all  #その際@booksが空の場合はBook.allを改めて代入、それ以外は何もしない
       @book = book #save予定だったブロック変数bookをindex.htmlのif文で使用するためインスタンス変数@bookに代入
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     if book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)  # <!--名前付きルート「as:'book'」→books#show→show.html.erb-->
+      redirect_to book_path(book.id)  #名前付きルート「as:'book'」→books#show→show.html.erb
     else
       @book = book
       render action: :edit
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
-    redirect_to books_path  # <!--名前付きルート「as:'books'」→books#index→index.html.erb-->
+    redirect_to books_path  #名前付きルート「as:'books'」→books#index→index.html.erb
   end
 
   private
